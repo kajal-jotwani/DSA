3294ecc40fd3799ca993e98403fbaa908510cddd
kajal-jotwani <kajaljotwani06@gmail.com>
2023-11-06 22:58:39 +0530

    done

--- Array/revrse.cpp
+++ Array/revrse.cpp
@@ -1,4 +1,35 @@
 #include<iostream>
+using namespace std;
+
+void reverse(int arr[], int n)
+{
+    int start=0;
+    int end=n-1;
+
+    while(start<=end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int arr[6]={2, 4, 5, 8, 9, 7};
+
+    reverse (arr, 6) ;
+
+    printArray(arr, 6);
+}
+
+// #include<iostream>
 // using namespace std;
 
 // void reverse(int arr[], int n) {
