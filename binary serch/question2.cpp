479763dff669b429e22b4b547c1f8d0723df7c0c
kajal-jotwani <kajaljotwani06@gmail.com>
2023-11-23 16:59:36 +0530

    done

---
+++ binary serch/question2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+// Function declarations
+int firstOcc(vector<int> &arr, int n, int key);
+int lastOcc(vector<int> &arr, int n, int key);
+pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k);
+
+int main() {
+    // Example usage
+    vector<int> arr = {1, 2, 4, 4, 4, 5, 6};
+    int key = 4;
+
+    pair<int, int> result = firstAndLastPosition(arr, arr.size(), key);
+
+    cout << "First Occurrence: " << result.first << endl;
+    cout << "Last Occurrence: " << result.second << endl;
+
+    return 0;
+}
+
+int firstOcc(vector<int> &arr, int n, int key) {
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+
+    while (s <= e) {
+        if (arr[mid] == key) {
+            ans = mid;
+            e = mid - 1;
+        } else if (key > arr[mid]) {
+            s = mid + 1;
+        } else if (key < arr[mid]) {
+            e = mid - 1;
+        }
+
+        mid = s + (e - s) / 2;
+    }
+
+    return ans;
+}
+
+int lastOcc(vector<int> &arr, int n, int key) {
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+
+    while (s <= e) {
+        if (arr[mid] == key) {
+            ans = mid;
+            s = mid + 1;
+        } else if (key > arr[mid]) {
+            s = mid + 1;
+        } else if (key < arr[mid]) {
+            e = mid - 1;
+        }
+
+        mid = s + (e - s) / 2;
+    }
+
+    return ans;
+}
+
+pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k) {
+    pair<int, int> p;
+    p.first = firstOcc(arr, n, k);
+    p.second = lastOcc(arr, n, k);
+
+    return p;
+}
