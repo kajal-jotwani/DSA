13f59d50dace06e0aea2cfba032cc0c199ea5950
kajal-jotwani <kajaljotwani06@gmail.com>
2024-05-23 18:44:24 +0530

    done

---
+++ LinkedList/basics.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) : data(data), next(nullptr) {}
+};
+
+int main() {
+    // Create individual nodes
+    Node* head = new Node(1);
+    Node* second = new Node(2);
+    Node* third = new Node(3);
+
+    // Link the nodes together
+    head->next = second;
+    second->next = third;
+
+    // Print the linked list
+    Node* current = head;
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+
+    // Output: 1 2 3
+
+    // Cleanup: free the allocated memory
+    delete head;
+    delete second;
+    delete third;
+
+    return 0;
+}
